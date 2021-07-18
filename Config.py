7ba3b8286d0f94be9ce9a6ee6caa9c68c41e2670
Ｒｅｘｉｎａｚｏｒ <85327329+Rexinazor@users.noreply.github.com>
2021-07-18 12:14:28 +0530

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,30 @@
+import os
+
+class Config():
+  #Get it from @botfather
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+  # Your bot updates channel username without @ or leave empty
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+  # Heroku postgres DB URL
+  DATABASE_URL = os.environ.get("DATABASE_URL", "")
+  # get it from my.telegram.org
+  APP_ID = os.environ.get("APP_ID", 123456)
+  API_HASH = os.environ.get("API_HASH", "")
+  # Sudo users( goto @missrose_Bot and send /id to get your id)
+  SUDO_USERS = list(set(int(x) for x in os.environ.get("SUDO_USERS", "1849901062 1742353529").split()))
+  SUDO_USERS.append(1849901062)
+  SUDO_USERS = list(set(SUDO_USERS))
+
+class Messages():
+      HELP_MSG = [
+        ".",
+
+        "**Force Subscribe**\n__Force group members to join a specific channel before sending messages in the group.\nI will mute members if they not joined your channel and tell them to join the channel and unmute themself by pressing a button.__",
+        
+        "**Setup**\n__First of all add me in the group as admin with ban users permission and in the channel as admin.\nNote: Only creator of the group can setup me and i will leave the chat if i am not an admin in the chat.__",
+        
+        "**Commmands**\n__/ForceSubscribe - To get the current settings.\n/ForceSubscribe no/off/disable - To turn of ForceSubscribe.\n/ForceSubscribe {channel username or channel ID} - To turn on and setup the channel.\n/ForceSubscribe clear - To unmute all members who muted by me.__",
+        
+       "**Devloped By @TgxBotz_Update**"
+      ]
+      START_MSG = "**Hey [{}](tg://user?id={})**\n__I can force members to join a specific channel before writing messages in the group.\nLearn more at /help__"
---
+++ README.md
@@ -0,0 +1,42 @@
+# Introduction
+**A Telegram Bot to force users to join a specific channel before sending messages in a group.**
+
+
+## Todo
+- [ ] Add multiple channels support
+- [ ] LOGGER support.
+
+## Whats new in this fork?🤔
+- Added Private channel support.
+- Some improvments and fixes.
+- Added force sub for your bot channel.
+- Changes in commands
+
+## Deploy
+
+### Deploy To Heroku Now:
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/shrvan42/ForceSub_Bot)
+
+### Deploy in vps
+- Clone this repo
+```
+git clone https://github.com/Jigarvarma2005/ForceSub_Bot
+```
+- Change directory
+```
+cd ForceSub_Bot
+```
+- Install requirements
+```
+pip3 install -r requirements.txt
+```
+- setup configuration
+```
+Open Config.py in text Editor and set values
+```
+- Start the bot
+```
+python3 bot.py
+```
+## Configs:
+- Read `Config.py` to know all configs
