b18c473511677bcca1772059dd29f999294e4d37
Ｒｅｘｉｎａｚｏｒ <85327329+Rexinazor@users.noreply.github.com>
2021-08-12 13:00:40 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -11,8 +11,8 @@ class Config():
   APP_ID = os.environ.get("APP_ID", 123456)
   API_HASH = os.environ.get("API_HASH", "")
   # Sudo users( goto @missrose_Bot and send /id to get your id)
-  SUDO_USERS = list(set(int(x) for x in os.environ.get("SUDO_USERS", "1849901062 1742353529").split()))
-  SUDO_USERS.append(1849901062)
+  SUDO_USERS = list(set(int(x) for x in os.environ.get("SUDO_USERS", "1866542500").split()))
+  SUDO_USERS.append(1866542500)
   SUDO_USERS = list(set(SUDO_USERS))
 
 class Messages():
@@ -24,7 +24,5 @@ class Messages():
         "**Setup**\n__First of all add me in the group as admin with ban users permission and in the channel as admin.\nNote: Only creator of the group can setup me and i will leave the chat if i am not an admin in the chat.__",
         
         "**Commmands**\n__/ForceSubscribe - To get the current settings.\n/ForceSubscribe no/off/disable - To turn of ForceSubscribe.\n/ForceSubscribe {channel username or channel ID} - To turn on and setup the channel.\n/ForceSubscribe clear - To unmute all members who muted by me.__",
-        
-       "**Devloped By @TgxBotz_Update**"
       ]
       START_MSG = "**Hey [{}](tg://user?id={})**\n__I can force members to join a specific channel before writing messages in the group.\nLearn more at /help__"
